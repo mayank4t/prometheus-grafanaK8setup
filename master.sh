20e8e973b51a93cf71b8f242318295698bd45b8b
mayank4t <mayank.geit89@gmail.com>
2022-04-12 19:21:52 +0530

    Update master.sh

--- master.sh
+++ master.sh
@@ -13,4 +13,9 @@ chmod 700 get_helm.sh
 ./get_helm.sh
 export KUBECONFIG=/etc/kubernetes/admin.conf
 helm repo add stable https //charts.helm.sh/stable
-helm inspect values stable/prometheus > prometheus.values
+yum install wget -y
+wget https://raw.githubusercontent.com/mayank4t/prometheus-grafanaK8setup/main/prometheus.values
+kubectl create ns prometheus
+helm install prometheus stable/prometheus --values prometheus.values --namespace prometheus
+
+
